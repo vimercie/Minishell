890b6df0144ac93a16739b7f0b7aaf455c3a5c04
mmajani42 <mmajani42@gmail.com>
2022-12-06 15:15:41 +0100

    test

--- inc/minishell.h
+++ inc/minishell.h
@@ -15,5 +15,13 @@
 # define MINISHELL_H
 
 # include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <dirent.h>
+# include <string.h>
+# include <errno.h>
+# include <readline/readline.h>
 
 #endif
