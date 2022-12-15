ea879a01df86f2ee2c1ebd1d7d0fdac1f55bbc18
vimercie <vincent.mrc@hotmail.com>
2022-12-15 21:03:06 +0100

    added .gitignore

--- src/main.c
+++ src/main.c
@@ -6,18 +6,22 @@
 /*   By: vimercie <vimercie@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 10:41:05 by vimercie          #+#    #+#             */
-/*   Updated: 2022/12/07 19:38:20 by vimercie         ###   ########.fr       */
+/*   Updated: 2022/12/15 20:59:57 by vimercie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-int	main(void)
+int	main(int argc, char *argv[], char *envp[])
 {
 	t_command	*cmd;
 
+	(void)argv;
+	(void)envp;
+	if (argc != 1)
+		return (0);
 	while (1)
-	{	
+	{
 		cmd = malloc(sizeof(t_command));
 		cmd->cmd = readline("GigaBash$ ");
 		parsing(cmd);
