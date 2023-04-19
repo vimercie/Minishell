36964aa068ebb7531855bae3ad787aec245f354b
mmajani42 <mmajani42@gmail.com>
2023-04-19 12:31:55 +0200

    echo n skip all N

--- src/b_in/sort.c
+++ src/b_in/sort.c
@@ -6,7 +6,7 @@
 /*   By: mmajani <mmajani@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/19 14:05:46 by mmajani           #+#    #+#             */
-/*   Updated: 2023/04/15 17:53:47 by mmajani          ###   ########lyon.fr   */
+/*   Updated: 2023/04/19 11:25:03 by mmajani          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,8 @@ void	index_env(t_env *env)
 		next = env;
 		while (next->next != NULL)
 		{
-			if (elem->name && next->name && ft_strcmp(elem->name, next->name) > 0)
+			if (elem->name && next->name
+				&& ft_strcmp(elem->name, next->name) > 0)
 				elem->index++;
 			next = next->next;
 		}
